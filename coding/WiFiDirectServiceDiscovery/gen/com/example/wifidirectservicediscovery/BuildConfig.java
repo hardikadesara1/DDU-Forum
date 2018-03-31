8706c64a0681b85db3947c6795d9453b1cbecfc3
Hardik Adesara <hardikadesara1@gmail.com>
2018-03-31 14:58:11 -0700

    Uploaded
    University Forum

---
+++ coding/WiFiDirectServiceDiscovery/gen/com/example/wifidirectservicediscovery/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.wifidirectservicediscovery;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
